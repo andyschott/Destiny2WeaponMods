1c7500106a86f8691c93ce76b4a1597a8deb38a3
Andy Schott <andy@andyschott.com>
2019-09-12 20:30:51 -0400

    create the project

---
+++ Destiny2WeaponMods/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Destiny2WeaponMods.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
